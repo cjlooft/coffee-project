18835cd659943e1565996d0620a108c6ffa291b9
c.josettelooft <cjlooft@gmail.com>
2018-08-22 15:10:38 -0500

    user search for a coffee works, but only manually  work in progress

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 "use strict";
 
 
-
 function renderCoffee(coffee) {
     var html = '<div class="coffee col-md-6">';
 
-        html += '<h2>' +coffee.name + '</h2>';
-        html += '<div class="text-md">' +coffee.roast + '</div>';
-        html += '</div>';
+    html += '<h2>' + coffee.name + '</h2>';
+    html += '<div class="text-md">' + coffee.roast + '</div>';
+    html += '</div>';
 
     return html;
 }
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for (var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -24,13 +23,40 @@ function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
+
+    if (selectedRoast === "all") {
+        filteredCoffees = coffees;
+    } else {
+        coffees.forEach(function (coffee) {
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
+            }
+        });
+    }
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+function searchCoffees() {
+
+    var searchTerm = document.getElementById("search").value;
+    var filteredCoffees = [];
+
+    coffees.forEach(function (coffee) {
+
+        if (coffee.name.includes(searchTerm)) {
+            filteredCoffees.push(coffee);
+
+        }
+    });
+
+
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+
+
+
+}
+
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
